b82067e5f41e5049a169d026be83a3d07079bc7e
konivex <47462070+konivex@users.noreply.github.com>
2019-03-02 14:37:52 +0300

    Add files via upload

--- src/ru/avalon/java/ocpjp/labs/Main.java
+++ src/ru/avalon/java/ocpjp/labs/Main.java
@@ -3,38 +3,32 @@ package ru.avalon.java.ocpjp.labs;
 import ru.avalon.java.ocpjp.labs.console.ConsoleUI;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import ru.avalon.java.ocpjp.labs.actions.Action;
-import ru.avalon.java.ocpjp.labs.actions.FileCopyAction;
-import ru.avalon.java.ocpjp.labs.actions.FileMoveAction;
 
 /**
  * Лабораторная работа №2
  * <p>
- * Курс: "DEV-OCPJP. Подготовка к сдаче сертификационных экзаменов серии Oracle
- * Certified Professional Java Programmer"
+ * Курс: "DEV-OCPJP. Подготовка к сдаче
+ * сертификационных экзаменов серии Oracle Certified
+ * Professional Java Programmer"
  * <p>
- * Тема: "Потоки исполнения (Threads) и многозадачность"
+ * Тема: "Потоки исполнения (Threads) и многозадачность" 
  *
  * @author Daniel Alpatov <danial.alpatov@gmail.com>
  */
 public class Main extends ConsoleUI<Commands> {
-
     /**
      * Точка входа в приложение.
-     *
-     * @param args
+     * 
+     * @param args 
      */
     public static void main(String[] args) {
         new Main().run();
     }
-
     /**
      * Конструктор класса.
      * <p>
-     * Инициализирует экземпляр базового типа с использоавнием перечисления
-     * {@link Commands}.
+     * Инициализирует экземпляр базового типа с использоавнием
+     * перечисления {@link Commands}.
      */
     Main() {
         super(Commands.class);
@@ -45,36 +39,25 @@ public class Main extends ConsoleUI<Commands> {
      */
     @Override
     protected void onCommand(Commands command) throws IOException {
-        Action cmd = new FileCopyAction();
+        System.out.println("Hello, world!!!");
         switch (command) {
             case copy:
                 /*
                  * TODO №6 Обработайте команду copy
                  */
-
-                cmd.start("copy");
-
                 break;
             case move:
                 /*
                  * TODO №7 Обработайте команду move
                  */
-
-                cmd.start("move");
                 break;
             case exit:
                 close();
                 break;
-            case delete:
-                cmd.start("delete");
-                break;
-            case create:
-                cmd.start("create");
-                break;
-            /*
+                /*
                  * TODO №9 Обработайте необработанные команды
-             */
+                 */
         }
     }
-
+    
 }
